236787474db0ce73267f4fa427bc344e304c98f1
Rebeca Madi <rebecamadioli@gmail.com>
2023-07-20 18:39:00 -0400

    Lab-7 1.0

---
+++ Lab-Android/LAb-Encapsulamento/src/br/edu/ufam/icomp/lab_encapsulamento/GISMain.java
@@ -0,0 +1,8 @@
+package br.edu.ufam.icomp.lab_encapsulamento;
+
+public class GISMain {
+	public static void main(String[] args) {
+		Posicao p = new Posicao(-3.089242, -59.964874, 88.374);
+		System.out.println(p.toString());
+	}
+}
